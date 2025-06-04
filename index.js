490dc8257fedbcfa7f52973bca64e3fc3550ea54
Eng. Odeh Alamrow <o_alamrow@hotmail.com>
2025-06-04 15:20:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json()); // <-- Allows ESP8266 to send JSON data
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  // <- Allows form data
 
-let sensorData = {}; // <-- Stores latest ESP8266 readings
+let sensorData = {};  
 
 app.post("/api/environment", (req, res) => {
     sensorData = req.body;
