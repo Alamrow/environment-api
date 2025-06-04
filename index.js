8c0de8a26aebb212938103ee33df8b9f76d8a685
Eng. Odeh Alamrow <o_alamrow@hotmail.com>
2025-06-04 15:27:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());  
-app.use(express.urlencoded({ extended: true }));  // <- Allows form data
+app.use(express.urlencoded({ extended: true }));  // <-- Ensures form data is handled properly
 
 let sensorData = {};  
 
 app.post("/api/environment", (req, res) => {
+    console.log("Received Data:", req.body);  // <-- Debugging output for Vercel logs
     sensorData = req.body;
     res.json({ message: "Data received!", sensorData });
 });
